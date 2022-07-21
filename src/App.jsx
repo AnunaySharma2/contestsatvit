c01d6d5f35efa4c30f93ee80262c130d13fd9c65
AnunaySharma <anunaysharma123456789@gmail.com>
2022-07-21 15:23:55 +0530

    theme and layout change

--- src/App.jsx
+++ src/App.jsx
@@ -18,30 +18,8 @@ function App() {
 
   return (
     <div className="m-5">
-      <h1 className="text-5xl font-bold">Upcoming Contests</h1>
+      <h1 className="text-5xl font-bold text-accent-focus">Upcoming Contests</h1>
       <div className="stats shadow mt-3">
-        <div className="stat">
-          <div className="stat-figure text-primary">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block w-8 h-8 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-              ></path>
-            </svg>
-          </div>
-          <div className="stat-title">Running Contests</div>
-          <div className="stat-value text-primary">
-            {runningContests && runningContests.length}
-          </div>
-        </div>
-
         <div className="stat">
           <div className="stat-figure text-secondary">
             <svg
@@ -58,7 +36,7 @@ function App() {
               ></path>
             </svg>
           </div>
-          <div className="stat-title">Upcoming Contests</div>
+          <div className="stat-title ">Upcoming Contests</div>
           <div className="stat-value text-secondary">
             {contestInfo && contestInfo.length}
           </div>
@@ -96,8 +74,30 @@ function App() {
             ))}
         </tbody>
       </table>
-      <h1 className="text-5xl font-bold">Running Contests</h1>
-
+      <h1 className="text-5xl font-bold text-accent-focus">Running Contests</h1>
+      <div className="stats shadow mt-3">
+        <div className="stat">
+          <div className="stat-figure text-primary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-8 h-8 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+              ></path>
+            </svg>
+          </div>
+          <div className="stat-title">Running Contests</div>
+          <div className="stat-value text-primary">
+            {runningContests && runningContests.length}
+          </div>
+        </div>
+      </div>
       <table className="table-zebra my-5 mx-auto w-full">
         <thead>
           <tr>
