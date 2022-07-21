ee5f16a4602d812f60b95e078654a2cb271896f3
AnunaySharma <anunaysharma123456789@gmail.com>
2022-07-21 15:03:55 +0530

    fixed typos and minor bugs

--- src/App.jsx
+++ src/App.jsx
@@ -14,54 +14,57 @@ function App() {
     };
 
     fetchData("https://kontests.net/api/v1/all");
-    console.log(contestInfo);
-    console.log(runningContests);
   }, []);
 
   return (
     <div className="m-5">
-      <h1 className="text-5xl font-bold">Running Contests</h1>
-      <div class="stats shadow mt-3">
-        <div class="stat">
-          <div class="stat-figure text-primary">
+      <h1 className="text-5xl font-bold">Upcoming Contests</h1>
+      <div className="stats shadow mt-3">
+        <div className="stat">
+          <div className="stat-figure text-primary">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              class="inline-block w-8 h-8 stroke-current"
+              className="inline-block w-8 h-8 stroke-current"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
               ></path>
             </svg>
           </div>
-          <div class="stat-title">Running Contests</div>
-          <div class="stat-value text-primary">{runningContests && runningContests.length}</div>
+          <div className="stat-title">Running Contests</div>
+          <div className="stat-value text-primary">
+            {runningContests && runningContests.length}
+          </div>
         </div>
 
-        <div class="stat">
-          <div class="stat-figure text-secondary">
+        <div className="stat">
+          <div className="stat-figure text-secondary">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              class="inline-block w-8 h-8 stroke-current"
+              className="inline-block w-8 h-8 stroke-current"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M13 10V3L4 14h7v7l9-11h-7z"
               ></path>
             </svg>
           </div>
-          <div class="stat-title">Upcoming Contests</div>
-          <div class="stat-value text-secondary">{contestInfo && contestInfo.length}</div>
+          <div className="stat-title">Upcoming Contests</div>
+          <div className="stat-value text-secondary">
+            {contestInfo && contestInfo.length}
+          </div>
         </div>
       </div>
+
       <table className="table-zebra my-5 mx-auto w-full">
         <thead>
           <tr>
@@ -71,8 +74,8 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {runningContests &&
-            runningContests.map((contest, idx) => (
+          {contestInfo &&
+            contestInfo.map((contest, idx) => (
               <tr key={idx}>
                 <td className="p-3 font-bold text-lg">
                   <a href={contest.url} target="blank">
@@ -93,7 +96,8 @@ function App() {
             ))}
         </tbody>
       </table>
-      <h1 className="text-5xl font-bold">Upcoming Contests</h1>
+      <h1 className="text-5xl font-bold">Running Contests</h1>
+
       <table className="table-zebra my-5 mx-auto w-full">
         <thead>
           <tr>
@@ -103,8 +107,8 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {contestInfo &&
-            contestInfo.map((contest, idx) => (
+          {runningContests &&
+            runningContests.map((contest, idx) => (
               <tr key={idx}>
                 <td className="p-3 font-bold text-lg">
                   <a href={contest.url} target="blank">
